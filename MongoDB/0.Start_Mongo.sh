17e4955dfa519cfba3e17f9b60016b028b882901
Jaejun <92967394+jaejunjeong@users.noreply.github.com>
2023-06-11 21:14:53 +0900

    Update 0.Start_Mongo.sh

--- MongoDB/0.Start_Mongo.sh
+++ MongoDB/0.Start_Mongo.sh
@@ -1,5 +1,23 @@
+##download mongoimport(for json)
+wget https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu1804-x86_64-100.3.1.tgz
+tar -xf mongodb-database-tools-ubuntu1804-x86_64-100.3.1.tgz
+export PATH=$PATH:/home/project/mongodb-database-tools-ubuntu1804-x86_64-100.3.1/bin
+echo "done"
+
+##check mongoimport version
+mongoimport --version
+
 ##start mongo
 start_mongo
+
+##import json file
+mongoimport -u root -p yourpassword --authenticationDatabase admin --db yourdb --collection yourcollection --file yourjsonfile.json
+
+##export json file
+mongoexport -u root -p yourpassword --authenticationDatabase admin --db yourdb --collection yourcollection --out yourjsonfile.json
+
+##export csv file
+mongoexport -u root -p yourpassword --authenticationDatabase admin --db yourdb --collection yourcollection --out yourcsvfile.csv --type=csv --fields _id,clarity,cut,price
 
 ##connect mongo as root
 mongo -u root -p password --authenticationDatabase admin local
